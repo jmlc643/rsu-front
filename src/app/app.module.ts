85237df0218680d0ddfb2a9c62e11c7eaed27244
jmlc643 <josemarialuyocampos@gmail.com>
2023-12-21 17:59:04 -0500

    Con fe funcione

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import {NgModule} from "@angular/core";
+import {AppComponent} from "./app.component";
+import {CommonModule} from "@angular/common";
+import {RouterOutlet} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AppRoutes} from "./app.routes";
+import {PatientFileApiService} from "../api/patient-file/patient-file-api.service";
+import {PatientApiService} from "../api/patient/patient-api.service";
+
+@NgModule({
+  declarations : [
+    AppComponent
+  ],
+  imports : [
+    CommonModule,
+    RouterOutlet,
+    HttpClientModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutes
+  ],
+  providers : [PatientFileApiService, PatientApiService]
+})
+export class AppModule{}
